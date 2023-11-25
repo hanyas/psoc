7878c46f1193410fec021f85e4751bdca892bd59
hanyas <hanyas@users.noreply.github.com>
2023-11-25 13:16:00 +0200

    add linear env and example

---
+++ examples/csmc_linear_ctl.py
@@ -0,0 +1,215 @@
+from typing import Dict
+from functools import partial
+
+import jax
+from jax import random as jr
+from jax import numpy as jnp
+
+import numpy as onp
+
+from flax import linen as nn
+from flax.training.train_state import TrainState
+
+import optax
+
+from psoc.algorithms import smc, csmc
+from psoc.environments import linear_env as linear
+
+import matplotlib.pyplot as plt
+import time as clock
+
+jax.config.update("jax_platform_name", "cpu")
+jax.config.update("jax_enable_x64", True)
+# jax.config.update("jax_disable_jit", True)
+
+
+@partial(jax.jit, static_argnums=(1, 2))
+def expectation(
+    key: jax.Array,
+    nb_particles: int,
+    nb_samples: int,
+    reference: jnp.ndarray,
+    params: Dict,
+    eta: float,
+):
+    prior, closedloop, log_obsrv = \
+        linear.create_env(params, eta)
+
+    def body(carry, args):
+        key, reference = carry
+        key, sub_key = jr.split(key, 2)
+        sample = csmc(
+            sub_key,
+            nb_steps,
+            nb_particles,
+            1,
+            reference,
+            prior,
+            closedloop,
+            log_obsrv,
+        )[-1]
+        return (key, sample), sample
+
+    _, samples = \
+        jax.lax.scan(body, (key, reference), (), length=nb_samples)
+    return samples
+
+
+def lower_bound(
+    states: jnp.ndarray,
+    next_states: jnp.ndarray,
+    params: Dict,
+    eta: float,
+):
+    _, closedloop, log_obsrv = linear.create_env(params, eta)
+    lls = linear.log_complete_likelihood(states, next_states, closedloop, log_obsrv)
+    return - jnp.sum(lls)
+
+
+@jax.jit
+def maximization(
+    opt_state: TrainState,
+    states: jnp.ndarray,
+    next_states: jnp.ndarray,
+    eta: float,
+):
+    def loss_fn(params):
+        loss = lower_bound(states, next_states, params, eta)
+        return loss
+
+    grad_fn = jax.value_and_grad(loss_fn)
+    loss, grads = grad_fn(opt_state.params)
+    return opt_state.apply_gradients(grads=grads), loss
+
+
+def create_pairs(samples: jnp.ndarray):
+    nb_samples, nb_steps, dim = samples.shape
+    states, next_states = samples[:, :-1, :], samples[:, 1:, :]
+    states = jnp.reshape(states, (nb_samples * (nb_steps - 1), dim))
+    next_states = jnp.reshape(next_states, (nb_samples * (nb_steps - 1), dim))
+    return states, next_states
+
+
+def compute_cost(
+    samples: jnp.ndarray,
+    params: Dict,
+    eta: float
+):
+    _, _, cost = linear.create_env(params, eta)
+    _, next_states = create_pairs(samples)
+    return jnp.mean(jax.vmap(cost)(next_states))
+
+
+def batcher(
+    key: jax.Array,
+    samples: jnp.ndarray,
+    batch_size: int,
+):
+    states, next_states = create_pairs(samples)
+
+    # Shuffle
+    batch_idx = jr.permutation(key, len(states))
+    batch_idx = onp.asarray(batch_idx)
+
+    # Skip incomplete batch
+    steps_per_epoch = len(states) // batch_size
+    batch_idx = batch_idx[: steps_per_epoch * batch_size]
+    batch_idx = batch_idx.reshape((steps_per_epoch, batch_size))
+
+    for idx in batch_idx:
+        yield states[idx], next_states[idx]
+
+
+def create_train_state(
+    key: jax.Array,
+    module: nn.Module,
+    learning_rate: float
+):
+    init_data = jnp.zeros((2,))
+    params = module.init(key, init_data)["params"]
+    tx = optax.adam(learning_rate)
+    return TrainState.create(
+        apply_fn=module.apply,
+        params=params,
+        tx=tx
+    )
+
+
+key = jr.PRNGKey(123)
+
+nb_steps = 51
+nb_particles = 512
+nb_samples = 50
+
+nb_iter = 15
+eta = 0.1
+
+key, sub_key = jr.split(key, 2)
+opt_state = create_train_state(sub_key, linear.network, 1e-3)
+
+start = clock.time()
+
+prior, closedloop, cost = \
+    linear.create_env(opt_state.params, eta)
+
+key, sub_key = jr.split(key, 2)
+samples, weights = smc(
+    sub_key,
+    nb_steps,
+    nb_particles,
+    nb_samples,
+    prior,
+    closedloop,
+    cost,
+)
+
+key, sub_key = jr.split(key, 2)
+idx = jr.choice(sub_key, a=nb_samples, p=weights)
+reference = samples[idx, :, :]
+
+# plt.plot(reference)
+# plt.show()
+
+for i in range(nb_iter):
+    key, estep_key, mstep_key, ref_key = jr.split(key, 4)
+
+    # expectation step
+    samples = expectation(
+        estep_key,
+        nb_particles,
+        nb_samples,
+        reference,
+        opt_state.params,
+        float(eta)
+    )
+
+    # maximization step
+    loss = 0.0
+    batches = batcher(mstep_key, samples, 64)
+    for batch in batches:
+        states, next_states = batch
+        opt_state, batch_loss = \
+            maximization(opt_state, states, next_states, eta)
+        loss += batch_loss
+
+    print(
+        f" iter: {i},"
+        f" loss: {loss},"
+        f" log_std: {opt_state.params['log_std']}"
+    )
+
+    # choose new reference
+    reference = samples[-1]
+
+
+# for n in range(nb_samples):
+#     plt.plot(samples[n, :, :])
+# plt.show()
+
+prior, closedloop, _ = \
+    linear.create_env(opt_state.params, eta)
+states = linear.simulate(prior, closedloop, nb_steps)
+
+plt.figure()
+plt.plot(states)
+plt.show()
---
+++ psoc/environments/linear_env.py
@@ -0,0 +1,122 @@
+from typing import Callable
+from functools import partial
+
+import jax
+
+from jax import numpy as jnp
+from jax import lax as jl
+
+import distrax
+from flax import linen as nn
+
+from psoc.abstract import StochasticDynamics
+from psoc.abstract import PolicyNetwork
+from psoc.abstract import StochasticPolicy
+from psoc.abstract import ClosedLoop
+
+from psoc.utils import Tanh
+
+jax.config.update("jax_enable_x64", True)
+
+
+@partial(jnp.vectorize, signature='(k),(h)->(k)')
+def ode(
+    x: jnp.ndarray, u: jnp.ndarray
+) -> jnp.ndarray:
+
+    A = jnp.array(
+        [
+            [0.0, 1.0],
+            [0.0, 0.0]
+        ]
+    )
+    B = jnp.array(
+        [
+            [0.0],
+            [1.0]
+        ]
+    )
+    return A @ x + B @ u
+
+
+@partial(jnp.vectorize, signature='(k),()->()')
+def cost(state, eta):
+    goal = jnp.array([0.0, 0.0, 0.0])
+    weights = jnp.array([1e1, 1e0, 1e0])
+    cost = - 0.5 * jnp.dot(state - goal, weights * (state - goal))
+    return eta * cost
+
+
+prior = distrax.MultivariateNormalDiag(
+    loc=jnp.array([1.0, 2.0, 0.0]),
+    scale_diag=jnp.ones((3,)) * 1e-4
+)
+
+dynamics = StochasticDynamics(
+    dim=2,
+    ode=ode,
+    step=0.1,
+    log_std=jnp.log(1e-2 * jnp.ones((2,)))
+)
+
+
+@partial(jnp.vectorize, signature='(k)->(h)')
+def identity(x):
+    return x
+
+
+network = PolicyNetwork(
+    dim=1,
+    layer_size=[],
+    transform=identity,
+    activation=nn.relu,
+    init_log_std=jnp.log(1.0 * jnp.ones((1,))),
+)
+
+bijector = distrax.Chain([
+    distrax.ScalarAffine(0.0, 2.5),
+    Tanh()
+])
+
+
+def create_env(params, eta):
+    policy = StochasticPolicy(
+        network, bijector, params
+    )
+
+    closedloop = ClosedLoop(
+        dynamics, policy
+    )
+
+    anon_cost = lambda z: cost(z, eta)
+    return prior, closedloop, anon_cost
+
+
+def simulate(
+    prior: distrax.Distribution,
+    transition_model: ClosedLoop,
+    length: int,
+):
+    def body(carry, args):
+        prev_state = carry
+        next_state = transition_model.mean(prev_state)
+        return next_state, next_state
+
+    init_state = prior.mean()
+    _, states = \
+        jl.scan(body, init_state, (), length=length - 1)
+
+    states = jnp.insert(states, 0, init_state, 0)
+    return states
+
+
+@partial(jax.vmap, in_axes=(0, 0, None, None))
+def log_complete_likelihood(
+    state: jnp.ndarray,
+    next_state: jnp.ndarray,
+    transition_model: ClosedLoop,
+    log_observation: Callable,
+):
+    ll = transition_model.logpdf(state, next_state) \
+         + log_observation(next_state)
+    return ll
