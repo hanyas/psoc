711eea713a19f93be332e2ff0b479bbc4ba4a01b
hanyas <hanyas@users.noreply.github.com>
2023-12-05 10:45:41 +0200

    refactor abstractions

--- psoc/abstract.py
+++ psoc/abstract.py
@@ -6,7 +6,7 @@ from jax import numpy as jnp
 import distrax
 from flax import linen as nn
 
-from psoc.utils import constrain
+from psoc.utils import identity_constraint
 
 
 class Network(nn.Module):
@@ -14,7 +14,7 @@ class Network(nn.Module):
     layer_size: Sequence[int]
     transform: Callable
     activation: Callable
-    init_log_std: jnp.ndarray
+    log_std_init: Callable = nn.initializers.ones
 
     @nn.compact
     def __call__(self, x):
@@ -24,12 +24,12 @@ class Network(nn.Module):
         u = nn.Dense(self.dim)(y)
 
         log_std = \
-            self.param('log_std', lambda rng, shape: self.init_log_std, 1)
+            self.param('log_std', self.log_std_init, 1)
 
         return u
 
 
-class FeedbackPolicy(NamedTuple):
+class FeedbackPolicyWithSquashing(NamedTuple):
     module: Network
     bijector: distrax.Chain
     params: Dict
@@ -66,49 +66,110 @@ class FeedbackPolicy(NamedTuple):
 
 class Gaussian(nn.Module):
     dim: int
-    init_params: jnp.ndarray
+    loc_init: Callable = nn.initializers.zeros
+    scale_init: Callable = nn.initializers.ones
 
     @nn.compact
     def __call__(self, u):
-        loc = self.param('loc', lambda rng, shape: self.init_params['loc'], 1)
-        scale = self.param('scale', lambda rng, shape: self.init_params['scale'], 1)
+        loc = self.param('loc', self.loc_init, self.dim)
+        scale = self.param('scale', self.scale_init, self.dim)
         return jnp.broadcast_to(loc, u.shape), jnp.broadcast_to(scale, u.shape)
+
+
+class TimeVariantGaussian(nn.Module):
+    dim: int
+    nb_steps: int
+    loc_init: Callable = nn.initializers.zeros
+    scale_init: Callable = nn.initializers.ones
+
+    @nn.compact
+    def __call__(self, u, k):
+        loc = self.param('loc', self.loc_init, (self.nb_steps, self.dim))
+        scale = self.param('scale', self.scale_init, (self.nb_steps, self.dim))
+        return jnp.broadcast_to(loc[k, :], u.shape), jnp.broadcast_to(scale[k, :], u.shape)
 
 
 class GaussMarkov(nn.Module):
     dim: int
     step: int
-    init_params: jnp.ndarray
+    inv_length_init: Callable = nn.initializers.ones
+    diffusion_init: Callable = nn.initializers.ones
 
     @nn.compact
     def __call__(self, u):
-        l = self.param('l', lambda rng, shape: self.init_params['l'], 1)
-        q = self.param('q', lambda rng, shape: self.init_params['q'], 1)
+        inv_length = self.param('inv_length', self.inv_length_init, self.dim)
+        diffusion = self.param('diffusion', self.diffusion_init, self.dim)
 
-        sigma_sqr = q / (2.0 * l) * (1.0 - jnp.exp(-2.0 * l * self.step))
+        sigma_sqr = diffusion / (2.0 * inv_length) \
+                    * (1.0 - jnp.exp(-2.0 * inv_length * self.step))
 
-        loc = jnp.exp(- l * self.step) * u
+        loc = jnp.exp(- inv_length * self.step) * u
         scale = jnp.sqrt(sigma_sqr)
         return loc, scale
 
 
-class OpenloopPolicy(NamedTuple):
-    module: Union[Gaussian, GaussMarkov]
+class OpenloopPolicyWithClipping(NamedTuple):
+    proposal: Gaussian
     bijector: distrax.Chain
     params: Dict
+    constraint: Callable = identity_constraint
 
     @property
     def dim(self):
-        return self.module.dim
+        return self.proposal.dim
 
     def mean(self, u):
-        _params = constrain(self.params)
-        un, _ = self.module.apply({'params': _params}, u)
+        _params = self.constraint(self.params)
+        un, _ = self.proposal.apply({'params': _params}, u)
         return self.bijector.forward(un)
 
     def distribution(self, u):
-        _params = constrain(self.params)
-        loc, scale = self.module.apply({'params': _params}, u)
+        _params = self.constraint(self.params)
+        loc, scale = self.proposal.apply({'params': _params}, u)
+
+        clipped_dist = distrax.ClippedNormal(
+            loc=loc,
+            scale=scale,
+            minimum=-1.0,
+            maximum=1.0
+        )
+
+        scaled_dist = distrax.Transformed(
+            distribution=distrax.Independent(
+                clipped_dist,
+                reinterpreted_batch_ndims=1
+            ),
+            bijector=distrax.Block(self.bijector, ndims=1)
+        )
+        return scaled_dist
+
+    def sample(self, key, u):
+        dist = self.distribution(u)
+        return dist.sample(seed=key)
+
+    def logpdf(self, u, un):
+        dist = self.distribution(u)
+        return dist.log_prob(un)
+
+
+class OpenloopPolicyWithSquashing(NamedTuple):
+    proposal: Union[Gaussian, GaussMarkov]
+    bijector: distrax.Chain
+    params: Dict
+    constraint: Callable = identity_constraint
+
+    @property
+    def dim(self):
+        return self.proposal.dim
+
+    def mean(self, u):
+        _params = self.constraint(self.params)
+        un, _ = self.proposal.apply({'params': _params}, u)
+        return self.bijector.forward(un)
+
+    def distribution(self, u):
+        _params = self.constraint(self.params)
+        loc, scale = self.proposal.apply({'params': _params}, u)
 
         raw_dist = distrax.MultivariateNormalDiag(
             loc=loc, scale_diag=jnp.atleast_1d(scale)
@@ -133,7 +194,7 @@ class StochasticDynamics(NamedTuple):
     dim: int
     ode: Callable
     step: float
-    log_std: jnp.ndarray
+    stddev: jnp.ndarray
 
     @staticmethod
     def runge_kutta(
@@ -160,7 +221,7 @@ class StochasticDynamics(NamedTuple):
     def distribution(self, x, u):
         return distrax.MultivariateNormalDiag(
             loc=self.mean(x, u),
-            scale_diag=jnp.exp(self.log_std)
+            scale_diag=self.stddev
         )
 
     def sample(self, key, x, u):
@@ -174,7 +235,7 @@ class StochasticDynamics(NamedTuple):
 
 class FeedbackLoop(NamedTuple):
     dynamics: StochasticDynamics
-    policy: FeedbackPolicy
+    policy: FeedbackPolicyWithSquashing
 
     @property
     def xdim(self):
@@ -224,7 +285,10 @@ class FeedbackLoop(NamedTuple):
 
 class OpenLoop(NamedTuple):
     dynamics: StochasticDynamics
-    policy: OpenloopPolicy
+    policy: Union[
+        OpenloopPolicyWithSquashing,
+        OpenloopPolicyWithClipping
+    ]
 
     @property
     def xdim(self):
